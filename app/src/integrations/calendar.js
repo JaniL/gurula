cf9f940cf8a691e90443abd317ecb4f01b5baa5e
Jani Luukko <jani.luukko@reaktor.com>
2017-10-08 22:58:06 +0300

    use R.values instead of mapping etc

--- app/src/integrations/calendar.js
+++ app/src/integrations/calendar.js
@@ -6,6 +6,6 @@ const R = require('ramda')
 module.exports = () =>
   axios.get(ICS_URL)
     .then(res => ical.parseICS(res.data))
-    .then(res => Object.keys(res).map(k => res[k]))
+    .then(R.values())
     .then(R.filter(R.propEq('type', 'VEVENT')))
     .then(R.sortBy(R.prop('start')))
